77cd91ae80b24cd3b7f7edd57f0a2123d71c4403
Edargorter <zdbowditch777@gmail.com>
2020-10-02 15:59:50 +0200

    Remaining

---
+++ Single_Byte_XOR_Cipher/decrypt.c
@@ -0,0 +1,13 @@
+#include "../lib/local_lib.h"
+
+char freq[] = "etoainsrhdlucmfywgpbvkxqjz";
+
+int main(int argc, char **argv)
+{
+	if(argc < 2){
+		printf("Usage: %s [ string ]\n", argv[0]);
+		exit(0);
+	}
+
+	return 0;
+}
